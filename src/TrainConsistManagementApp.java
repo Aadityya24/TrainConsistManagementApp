7afcd8bfd5874997e6b5e0c84752711188c7e273
Aadityya24 <aadityyaggholasgaon@gmail.com>
2026-04-07 09:50:44 +0530

    UC7: Sort bogies by capacity using Comparator

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,38 +1,44 @@
 import java.util.*;
 
+class PassengerBogie {
+    String id;
+    String type;
+    int capacity;
+
+    PassengerBogie(String id, String type, int capacity) {
+        this.id = id;
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    void display() {
+        System.out.println(id + " | " + type + " | " + capacity);
+    }
+}
+
+class CapacityComparator implements Comparator<PassengerBogie> {
+    public int compare(PassengerBogie a, PassengerBogie b) {
+        return b.capacity - a.capacity;
+    }
+}
+
 class Train {
-    HashMap<String, Integer> bogieMap = new HashMap<>();
+    ArrayList<PassengerBogie> bogies = new ArrayList<>();
 
-    void addBogie(String id, int capacity) {
-        if (bogieMap.containsKey(id)) {
-            System.out.println("Duplicate ID not allowed");
-        } else {
-            bogieMap.put(id, capacity);
-            System.out.println("Bogie added");
-        }
-    }
-
-    void removeBogie(String id) {
-        if (bogieMap.containsKey(id)) {
-            bogieMap.remove(id);
-            System.out.println("Bogie removed");
-        } else {
-            System.out.println("Bogie not found");
-        }
-    }
-
-    void searchBogie(String id) {
-        if (bogieMap.containsKey(id)) {
-            System.out.println("Capacity: " + bogieMap.get(id));
-        } else {
-            System.out.println("Bogie not found");
-        }
+    void addBogie(PassengerBogie b) {
+        bogies.add(b);
+        System.out.println("Bogie added");
     }
 
     void displayAll() {
-        for (Map.Entry<String, Integer> entry : bogieMap.entrySet()) {
-            System.out.println(entry.getKey() + " | Capacity: " + entry.getValue());
+        for (PassengerBogie b : bogies) {
+            b.display();
         }
+    }
+
+    void sortByCapacity() {
+        Collections.sort(bogies, new CapacityComparator());
+        System.out.println("Sorted by capacity");
     }
 }
 
@@ -46,10 +52,9 @@ public class TrainConsistManagementApp {
 
         do {
             System.out.println("\n1 Add Bogie");
-            System.out.println("2 Remove Bogie");
-            System.out.println("3 Search Bogie");
-            System.out.println("4 Display All");
-            System.out.println("5 Exit");
+            System.out.println("2 Display All");
+            System.out.println("3 Sort by Capacity");
+            System.out.println("4 Exit");
 
             choice = sc.nextInt();
 
@@ -58,30 +63,22 @@ public class TrainConsistManagementApp {
                     sc.nextLine();
                     System.out.print("Enter ID: ");
                     String id = sc.nextLine();
+                    System.out.print("Enter type: ");
+                    String type = sc.nextLine();
                     System.out.print("Enter capacity: ");
                     int cap = sc.nextInt();
-                    train.addBogie(id, cap);
+                    train.addBogie(new PassengerBogie(id, type, cap));
                     break;
 
                 case 2:
-                    sc.nextLine();
-                    System.out.print("Enter ID: ");
-                    String removeId = sc.nextLine();
-                    train.removeBogie(removeId);
+                    train.displayAll();
                     break;
 
                 case 3:
-                    sc.nextLine();
-                    System.out.print("Enter ID: ");
-                    String searchId = sc.nextLine();
-                    train.searchBogie(searchId);
-                    break;
-
-                case 4:
-                    train.displayAll();
+                    train.sortByCapacity();
                     break;
             }
 
-        } while (choice != 5);
+        } while (choice != 4);
     }
 }
